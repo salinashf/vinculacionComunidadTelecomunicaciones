04bbdf7b664bc4e6a4a7e1f2c8cebcf0250c9455
henry <salinashf@gmail.com>
2019-12-04 14:48:00 -0500

    Ajuste al   diseño de la  paguina

--- js/functionMath.js
+++ js/functionMath.js
@@ -10,3 +10,50 @@ function SIN(value) {
 function COS(value) {
     return Math.cos(value);
 }
+
+var  formatAmplitud = function (value) {
+        return value + "m" //String.fromCharCode(0x0041);
+};
+var  formatFrecuencia = function (value) {
+    return value +  "Hz"//String.fromCharCode(0x3390);
+
+};
+
+var confAmpPortadora ={
+    "displayPrevious":true,
+    "fgColor":"#1da780",
+    "inputColor":"#fe081c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    'format': formatAmplitud
+};
+var confFrecPortadora ={
+    "displayPrevious":true,
+    "fgColor":"#d8bd2d",
+    "inputColor":"#fe081c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    'format': formatFrecuencia
+};
+//######################
+var confAmpModuladora ={
+    "displayPrevious":true,
+    "fgColor":"#35ecfe",
+    "inputColor":"#fe081c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    'format': formatAmplitud
+};
+var confFrecModuladora ={
+    "displayPrevious":true,
+    "fgColor":"#23b3fe",
+    "inputColor":"#fe081c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    'format': formatFrecuencia
+};
+
--- js/main.js
+++ js/main.js
@@ -2,30 +2,20 @@ var theme = 'dark';
 
 (function () {
     'use strict';
-    var section = (document.location.hash) ? document.location.hash.slice(1) : 'ask';
-    $('#trunk').load('charts/' + section + '.htm', function () {
-        $('pre code').each(function (i, block) {
-            hljs.highlightBlock(block);
-        });
-    });
+    var section =  'ask';
+    $('#trunk').load('charts/' + section + '.htm');
     $('#mainHeader li a#goto-' + section).addClass('active');
     assignEventListeners();
-
     function assignEventListeners() {
         $('#mainHeader li a.pill').on('click', function (event) {
             event.preventDefault();
             $('#mainHeader li a.pill').removeClass('active');
             $(this).addClass('active');
             var section = $(this).attr('id').slice(5);
-            $('#trunk').load('charts/' + section + '.htm', function () {
-                $('pre code').each(function (i, block) {
-                    hljs.highlightBlock(block);
-                });
-            });
+            $('#trunk').load('charts/' + section + '.htm');
             document.location.hash = section;
             return false;
         });
-
         $('#dark-css').on('click', function () {
             theme = 'dark';
 
@@ -41,7 +31,7 @@ var theme = 'dark';
             $('.mg-missing-background')
                 .css('stroke', '#ccc');
 
-            $('.head ul li a.pill').removeClass('active');
+            $('.head ul li a.pillHeader').removeClass('active');
             $(this).toggleClass('active');
             $('#dark').attr({href: 'css/metricsgraphics-demo-dark.css'});
             $('#dark-code').attr({href: 'css/railscasts.css'});
@@ -49,7 +39,6 @@ var theme = 'dark';
 
             return false;
         });
-
         $('#light-css').on('click', function () {
             theme = 'light';
 
@@ -65,7 +54,7 @@ var theme = 'dark';
             $('.mg-missing-background')
                 .css('stroke', 'blue');
 
-            $('.head ul li a.pill').removeClass('active');
+            $('.head ul li a.pillHeader').removeClass('active');
             $(this).toggleClass('active');
             $('#dark').attr({href: ''});
             $('#dark-code').attr({href: ''});
@@ -75,7 +64,7 @@ var theme = 'dark';
         });
 
         $('#accessible-css').on('click', function () {
-            $('.head ul li a.pill').removeClass('active');
+            $('.head ul li a.pillHeader').removeClass('active');
             $(this).toggleClass('active');
             $('#accessible').attr({href: 'css/metricsgraphics-demo-accessible.css'});
 
@@ -84,24 +73,17 @@ var theme = 'dark';
     }
 })();
 var isLoadin = false;
-
 function loading() {
     if (!isLoadin) {
         isLoadin = true;
         EasyLoading.show({
             text: "dibujando..."
         });
-
     }
-
 }
-
 function dismiss() {
     if (isLoadin) {
         isLoadin = false;
         EasyLoading.hide();
-
     }
-
-
 }
