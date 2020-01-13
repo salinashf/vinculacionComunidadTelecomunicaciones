fbc7878ddc90bf9424803ef279e11494ad33df0d
henry <salinashf@gmail.com>
2020-01-13 17:17:43 -0500

    Ajsute  final de datos

--- js/functionMath.js
+++ js/functionMath.js
@@ -12,16 +12,24 @@ function COS(value) {
 }
 
 var  formatAmplitud = function (value) {
-        return value + "m" //String.fromCharCode(0x0041);
+        return value + "v" //String.fromCharCode(0x0041);
 };
 var  formatFrecuencia = function (value) {
-    return value +  "Hz"//String.fromCharCode(0x3390);
+    return value +  "kHz"//String.fromCharCode(0x3390);
 
 };
 var  formatDesviacion = function (value) {
     return value + String.fromCharCode(0x03c3); ///&sigma;
 
 };
+
+function limpiarControles( value ) {
+    if (value.is("input")){
+        return value.val().replace(/Hz|m|kHz|v/g, "");
+    }
+    return value.text().replace(/Hz|m|kHz|v/g, "");
+}
+
 var confAmpPortadora ={
     "displayPrevious":true,
     "fgColor":"#1da780",
@@ -77,3 +85,49 @@ var confFrecModuladora ={
     'release': null
 };
 
+var confAmpI={
+    "displayPrevious":true,
+    "fgColor":"#19a712",
+    "inputColor":"#fe081c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    'format': formatAmplitud,
+    'release': null
+};
+var confAmpQ={
+    "displayPrevious":true,
+    "fgColor":"#4c39a7",
+    "inputColor":"#fe081c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    'format': formatAmplitud,
+    'release': null
+};
+
+var confBitC0L = {
+    "displayPrevious":true,
+    "fgColor":"#b12536",
+    "inputColor":"#2efe1c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    "step":0.5,
+    "min" :0,
+    "max" :20,
+    'release': null
+};
+
+var confBitC1L = {
+    "displayPrevious":true,
+    "fgColor":"#d8bd2d",
+    "inputColor":"#2efe1c",
+    "thickness":.2,
+    "width":50,
+    "height":50,
+    "min" :0,
+    "max" :20,
+    "step":0.5,
+    'release': null
+};
